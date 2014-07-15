0cc113f3f3ebde3941536a3e9539f82dee16cb5c
phischu <pschuster@uni-koblenz.de>
2014-07-15 20:24:43 +0200

    disregard pipes

--- src/Haxl/Neo4j.hs
+++ src/Haxl/Neo4j.hs
@@ -5,13 +5,23 @@ module Haxl.Neo4j where
 
 import Haxl.Prelude
 
-import Pipes
 import Haxl.Core
 import Data.Typeable
 import Data.Hashable
 import Control.Exception
 
 type Haxl = GenHaxl ()
+newtype Neo4j a = Neo4j {unNeo4j :: Haxl [a]}
+
+instance Monad Neo4j where
+    return = Neo4j . return . (:[])
+    ma >>= amb = Neo4j (bind (unNeo4j ma) (unNeo4j . amb))
+
+bind :: Haxl [a] -> (a -> Haxl [b]) -> Haxl [b]
+bind has ahbs = do
+    as <- has
+    bss <- forM as ahbs
+    return (concat bss)
 
 data Neo4jReq a where
     Get :: Int -> Neo4jReq [Int]
@@ -44,14 +54,20 @@ neo4jFetch = asyncFetch
 actuallyFetch :: Neo4jReq a -> IO (IO (Either SomeException a))
 actuallyFetch (Get i) = return (return (Right [i+x | x <- [1..4]]))
 
-get :: Int -> Haxl [Int]
-get = dataFetch . Get
+gather :: Neo4j a -> Neo4j [a]
+gather = Neo4j . (fmap (:[])) . unNeo4j
+
+scatter :: [a] -> Neo4j a
+scatter as = Neo4j (return as)
+
+get :: Int -> Neo4j Int
+get i = Neo4j (dataFetch (Get i))
+
+runNeo4j :: Neo4j a -> IO [a]
+runNeo4j neo4j = do
+    env <- initEnv (stateSet Neo4jState stateEmpty) ()
+    runHaxl env (unNeo4j neo4j)
 
 main :: IO ()
-main = do
-    env <- initEnv (stateSet Neo4jState stateEmpty) ()
-    res <- runHaxl env (do
-        ids <- get 5
-        forM ids get)
-    print res
+main = runNeo4j (get 5 >>= get) >>= print
 
